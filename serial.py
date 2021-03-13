0e034762a314518d57ceeefbff81974e95cffed0
Nabin <nkbhaie2017@gmail.com>
2021-03-13 09:36:24 -0700

    add classes for object oriented programming in python

--- serial.py
+++ serial.py
@@ -19,12 +19,22 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
-    def __init__(self, start):
+    def __init__(self, start = 0):
+        """ Create a instance generator at given starting point"""
         self.start = start
+        #self.count = count
+        self.count = start
     
-    def generator(self):
-        # for i in range(start, ):
-        return start + 1
+        def generate(self):
+            """ It should return next serial number accompanying start"""
+            #self.count +=  1
+            self.count = count + 1
+            # return self.start + 1
+            return self.count - 1
+        
+        def reset(self):
+            """Reset start point to the original start number."""
+            self.count = self.start
 
 
 
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,28 @@
-"""Word Finder: finds random words from a dictionary."""
 
+from random import choice
 
 class WordFinder:
-    ...
+    """ Word Finder: finds random words from a dictionary.
+    
+    >>> find_random_word_count = WordFinder("my_text.txt")
+    14 words read
+
+    >>> find_random_word_count.random() in ["python"]
+    True
+    """
+    
+    def __init__(self, file):
+        """ Count number of words in a file"""
+        file_read = open(file)
+        self.file_words = self.func_to_read_file(file_read)
+    
+    def func_to_read_file(self, file_read):
+        """ To retrieve words in a file """
+        # remove all white spaces, and only retrieve words
+        for word in file_read:
+            return word.strip()
+    
+    def random_words(self):
+        """ Generate random words """
+        words = choice(self.file_words)
+        return words
