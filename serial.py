96a8f2cf76b1e8035a5565f7f12c5268cac623f4
Nabin <nkbhaie2017@gmail.com>
2021-03-13 06:36:46 -0700

    add all files to repo

---
+++ serial.py
@@ -0,0 +1,30 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+    def __init__(self, start):
+        self.start = start
+    
+    def generator(self):
+        # for i in range(start, ):
+        return start + 1
+
+
+
